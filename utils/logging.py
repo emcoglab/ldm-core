a71b320b96957a3961b2d1544d41163ba3d854b2
Cai <c.wingfield@lancaster.ac.uk>
2019-04-08 10:40:00 +0100

    Option to print progress bar

--- utils/logging.py
+++ utils/logging.py
@@ -1,2 +1,26 @@
 log_message = '%(asctime)s | %(levelname).2s | %(filename)s:%(lineno)d | \t%(message)s'
 date_format = "%Y-%m-%d %H:%M:%S"
+
+
+def print_progress(iteration, total, prefix='', suffix='', decimals=1, bar_length=100):
+    """
+    Call in a loop to create terminal progress bar.
+    Thanks to https://gist.github.com/aubricus/f91fb55dc6ba5557fbab06119420dd6a
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        bar_length  - Optional  : character length of bar (Int)
+    """
+    str_format = "{0:." + str(decimals) + "f}"
+    percents = str_format.format(100 * (iteration / float(total)))
+    filled_length = int(round(bar_length * iteration / float(total)))
+    bar = '█' * filled_length + '-' * (bar_length - filled_length)
+
+    stdout.write('\r%s |%s| %s%s %s' % (prefix, bar, percents, '%', suffix)),
+
+    if iteration == total:
+        stdout.write('\n')
+    stdout.flush()
